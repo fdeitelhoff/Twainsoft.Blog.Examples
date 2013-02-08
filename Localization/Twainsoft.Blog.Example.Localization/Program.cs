1dba2b739537f357c3b7d09dd18b88911effba43
Fabian Deitelhoff <github@fabiandeitelhoff.de>
2013-02-08 20:31:07 +0100

    Deleted unnecessary files in the EasierAppSettings project. Added the Localization project.

---
+++ Localization/Twainsoft.Blog.Example.Localization/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Twainsoft.Blog.Example.Localization
+{
+    static class Program
+    {
+        static void Main()
+        {
+            const string lowerCharacters = "abcdefghijklmnopqrstuvwxyz";
+            const string upperCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            var cultures = CultureInfo.GetCultures(CultureTypes.AllCultures);
+            foreach (var culture in cultures)
+            {
+                var upper = lowerCharacters.ToUpper(culture);
+                var lower = upperCharacters.ToLower(culture);
+
+                if (!upperCharacters.Equals(upper) || !lowerCharacters.Equals(lower))
+                {
+                    Console.WriteLine(culture.Name);
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Total cultures: {0}", cultures.Length);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Localization/Twainsoft.Blog.Examples.Localization.Tests/LocalizationTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Twainsoft.Blog.Examples.Localization.Tests
+{
+    [TestClass]
+    public class LocalizationTests
+    {
+        [TestMethod]
+        public void TestTurkishStringCompare()
+        {
+            const string lowerCharacters = "abcdefghijklmnopqrstuvwxyz";
+            const string upperCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            var turkishCulture = CultureInfo.GetCultureInfo("tr-TR");
+
+            var upper = lowerCharacters.ToUpper(turkishCulture);
+            var lower = upperCharacters.ToLower(turkishCulture);
+
+            Assert.AreNotEqual(upper, lower);
+        }
+    }
+}
