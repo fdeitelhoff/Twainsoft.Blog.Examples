77a575f95f3123bea2a9e9c41a40cc7c561ddb68
Fabian Deitelhoff <fabian@fabiandeitelhoff.de>
2013-11-02 00:40:32 +0100

    First calculations to mark ranges of years.

--- ExcelLifeBook/Twainsoft.Blog.Examples.ExcelLifeBook/LifeBook.cs
+++ ExcelLifeBook/Twainsoft.Blog.Examples.ExcelLifeBook/LifeBook.cs
@@ -1,4 +1,6 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
 using Microsoft.Office.Interop.Excel;
 using Office = Microsoft.Office.Core;
 
@@ -6,23 +8,72 @@ namespace Twainsoft.DNP.Articles.ExcelLifeBook
 {
     public partial class LifeBook
     {
-        private void Tabelle1_Startup(object sender, System.EventArgs e)
-        {
-        }
+        private void Tabelle1_Startup(object sender, System.EventArgs e) { }
 
-        private void Tabelle1_Shutdown(object sender, System.EventArgs e)
-        {
-        }
+        private void Tabelle1_Shutdown(object sender, System.EventArgs e) { }
 
         public void CreateLifeBook()
         {
             const int rows = 170;
             const int columns = 170;
 
-            // Mark 
+            // Create the table format.
             ApplyFormatting(rows, columns);
 
+            // Calculate and mark the birthdays with green cells.
             CalculateBirthdays(rows, columns);
+
+            // Mark the time of kindergarden.
+            MarkKinderkarden(3, 6);
+
+            // Mark the time of elementary school.
+            MarkElementarySchool(6, 10);
+
+            // Mark the time of middle school.
+            MarkMiddleSchool(10, 16);
+        }
+
+        private void MarkMiddleSchool(int fromYear, int toYear)
+        {
+            MarkRange(fromYear, toYear, Color.DarkOrange);
+        }
+
+        private void MarkElementarySchool(int fromYear, int toYear)
+        {
+            MarkRange(fromYear, toYear, Color.IndianRed);
+        }
+
+        private void MarkKinderkarden(int fromYear, int toYear)
+        {
+            MarkRange(fromYear, toYear, Color.SteelBlue);
+        }
+
+        
+
+        private void MarkRange(int fromYear, int toYear, Color color)
+        {
+            var dayStart = fromYear*365;
+            var dayEnd = toYear*365;
+
+            var startRow = Math.Ceiling((dayStart/170.0f));
+            var startColumn = dayStart%170;
+
+            for (var day = dayStart; day < dayEnd; day++)
+            {
+                var cell = Cells[startRow, startColumn++] as Range;
+                if (cell != null) cell.Interior.Color = color;
+
+                if (startColumn > 170)
+                {
+                    startColumn = 1;
+                    startRow++;
+
+                    if (startRow > 170)
+                    {
+                        startRow = 1;
+                    }
+                }
+            }
         }
 
         private void CalculateBirthdays(int rows, int columns)
@@ -36,7 +87,9 @@ namespace Twainsoft.DNP.Articles.ExcelLifeBook
                 {
                     var cell = Cells[row, column] as Range;
 
-                    if (days%365 == 0)
+                    //cell.Value2 = "-";
+
+                    if (cell != null && days % 365 == 0)
                     {
                         cell.Interior.Color = Color.Green;
                         Cells[row, column] = years;
@@ -56,7 +109,7 @@ namespace Twainsoft.DNP.Articles.ExcelLifeBook
             for (var i = 1; i <= 170; i++)
             {
                 var column = Columns[i, System.Type.Missing] as Range;
-                column.EntireColumn.ColumnWidth = 2.14;
+                if (column != null) column.EntireColumn.ColumnWidth = 2.14;
             }
         }
 
