6c029f761b04b1a5182a8dabf48daed8db668070
Fabian Deitelhoff <fabian@fabiandeitelhoff.de>
2013-11-02 11:58:58 +0100

    Added all color markers for the different life spans.

--- ExcelLifeBook/Twainsoft.Blog.Examples.ExcelLifeBook/LifeBook.cs
+++ ExcelLifeBook/Twainsoft.Blog.Examples.ExcelLifeBook/LifeBook.cs
@@ -18,17 +18,57 @@ namespace Twainsoft.Blog.Examples.ExcelLifeBook
             // Create the table format.
             ApplyFormatting(rows, columns);
 
-            // Calculate and mark the birthdays with green cells.
+            // Calculate and mark all birthdays with green cells.
             CalculateBirthdays(rows, columns);
 
-            // Mark the time of kindergarden.
+            // Mark the kindergarden time.
             MarkKinderkarden(3, 6);
 
-            // Mark the time of elementary school.
+            // Mark the elementary school time.
             MarkElementarySchool(6, 10);
 
-            // Mark the time of middle school.
+            // Mark the middle school time.
             MarkMiddleSchool(10, 16);
+
+            // Mark the high school time.
+            MarkHighSchool(16, 20);
+
+            // Mark the apprenticeship time.
+            MarkApprenticeship(20, 23);
+
+            // Mark the work time.
+            MarkWork(23, 26);
+
+            // Mark the bachelor time.
+            MarkBachelor(26, 29);
+
+            // Mark the master time.
+            MarkMaster(29, 31);
+        }
+
+        private void MarkMaster(int fromYear, int toYear)
+        {
+            MarkRange(fromYear, toYear, Color.Crimson);
+        }
+
+        private void MarkBachelor(int fromYear, int toYear)
+        {
+            MarkRange(fromYear, toYear, Color.Orange);
+        }
+
+        private void MarkWork(int fromYear, int toYear)
+        {
+            MarkRange(fromYear, toYear, Color.DarkGreen);
+        }
+
+        private void MarkApprenticeship(int fromYear, int toYear)
+        {
+            MarkRange(fromYear, toYear, Color.DarkMagenta);
+        }
+
+        private void MarkHighSchool(int fromYear, int toYear)
+        {
+            MarkRange(fromYear, toYear, Color.SlateBlue);
         }
 
         private void MarkMiddleSchool(int fromYear, int toYear)
